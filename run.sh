4a743ccabdf3ddd3e0fc16e8670f684d29afc067
Michael Willian Santos <dax-soft@live.com>
2025-01-16 19:27:27 -0300

    d

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 python tortoise/do_tts.py --text "The relentless northern weather shifted once again, the wind's mournful howl echoing through the desolate landscape as thick, heavy snowflakes spiraled down from a sky veiled in iron-gray clouds." --voice william --preset fast
+
+
+python tortoise/read_fast.py --textfile data/prologue.txt --voice william
