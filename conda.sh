c8045270fd721bcd8fbcd47d860a2a371ef8be9d
Michael Willian Santos <dax-soft@live.com>
2025-01-16 17:56:45 -0300

    d

---
+++ conda.sh
@@ -0,0 +1,7 @@
+conda activate tortoise
+
+# time python tortoise/do_tts.py \
+#     --output_path /results \
+#     --preset ultra_fast \
+#     --voice geralt \
+#     --text "Time flies like an arrow; fruit flies like a bananna."
---
+++ run.sh
@@ -0,0 +1,5 @@
+time python tortoise/do_tts.py \
+    --output_path /results \
+    --preset ultra_fast \
+    --voice geralt \
+    --text "Time flies like an arrow; fruit flies like a bananna."
